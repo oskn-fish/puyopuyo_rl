a327e86f5ffcffcb2675cbd7b7c16f4ef81db337
Shunnoske Chinen <flash_wave@icloud.com>
2023-08-05 01:21:35 +0900

    puyo movement too fast, vertical grid completed

--- puyopuyo.py
+++ puyopuyo.py
@@ -1,50 +1,80 @@
 import pygame
 import sys
+from pygame.locals import *
 
 pygame.init()
 
-COL_BLAC = pygame.Color((0, 0, 0))
-DISPLAY_SIZE = (400, 600)
+IMG_WIDTH = IMG_HEIGHT = 32
+BLACK = pygame.Color((0, 0, 0))
+DISPLAY_SIZE = (IMG_WIDTH*6, IMG_WIDTH*(12+1+3))
 DISPLAY = pygame.display.set_mode(DISPLAY_SIZE)
 CLOCK = pygame.time.Clock()
+pygame.key.set_repeat(100000,1000)
 
+
+pygame.display.set_caption("puyopuyo")
+
+# multiple puyos
+
+# user defined events
+# sprites group
+# sprite collision
+# move sprites in the group
+
+
+# single puyo
 class Puyo(pygame.sprite.Sprite):
     to_img = ({"red":"./img/0_0.png", 
                "green":"./img/0_1.png", 
                "blue":"./img/0_2.png", 
                "yellow":"./img/0_3.png", 
                "purple":"./img/0_4.png"})
-    def __init__(self, color: str) -> None:
+    def __init__(self, color: str, surface: pygame.surface) -> None:
         super().__init__()
         self.image = pygame.image.load(Puyo.to_img[color])
-        self.rect = self.image.get_rect(center = (DISPLAY_SIZE[0]//2, self.image.get_height()//2))
+        self.rect = self.image.get_rect(center = (2*IMG_WIDTH+IMG_WIDTH//2, self.image.get_height()//2))
+        self.surface = surface
     
-    def fall(self):
-        if self.rect.bottom < DISPLAY.get_height():
+    def fall(self) -> None:
+        if self.rect.bottom < self.surface.get_height():
             self.rect.move_ip(0, 5)
+        
+    def update(self):
+        self.fall()
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_LEFT] and self.rect.left>IMG_WIDTH//2:
+            self.rect.move_ip(-32, 0)
+        if pressed_keys[K_RIGHT]and self.rect.right<self.surface.get_width()-IMG_WIDTH//2:
+            self.rect.move_ip(32, 0)
         # pass
         
-    def draw(self, surface: pygame.Surface) -> None:
+    def draw(self) -> None:
         # pygame.Surface.blit(drawing_surface, destination_surface)
-        surface.blit(self.image, self.rect)
-        
+        self.surface.blit(self.image, self.rect)
+
+def draw_grid(surface: pygame.surface) -> None:
+    for i in range(1,6):
+        pygame.draw.line(surface, BLACK, (i*IMG_WIDTH, 0), (i*IMG_WIDTH, surface.get_height()))
+    
 
 def main():
-    puyo = Puyo("red")
+    puyo = Puyo("red", DISPLAY)
     
     
     while True:
         # catch events
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == QUIT:
                 # Simply using sys.exit() can cause your IDE to hang due to a common bug. 
                 pygame.quit()
                 sys.exit()
                 
         # update display
         DISPLAY.fill("white")
-        puyo.fall()
-        puyo.draw(DISPLAY)
+        draw_grid(DISPLAY)
+        # puyo.fall()
+        puyo.update()
+        puyo.draw()
         
         # render
         pygame.display.update()
