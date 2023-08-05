ed50fb5ab9012003743e997dc1bb63d883ac14a8
Shunnoske Chinen <flash_wave@icloud.com>
2023-08-05 18:02:27 +0900

    implemented FallingPuyos, added  sprites_group

--- puyopuyo.py
+++ puyopuyo.py
@@ -6,10 +6,14 @@ pygame.init()
 
 IMG_WIDTH = IMG_HEIGHT = 32
 BLACK = pygame.Color((0, 0, 0))
-DISPLAY_SIZE = (IMG_WIDTH*6, IMG_HEIGHT*(12+1+3))
-DISPLAY = pygame.display.set_mode(DISPLAY_SIZE)
-CLOCK = pygame.time.Clock()
 FALL_SPEED = 3
+display_size = (IMG_WIDTH*6, IMG_HEIGHT*(12+1+3))
+display = pygame.display.set_mode(display_size)
+clock = pygame.time.Clock()
+outlet_position = (2*IMG_WIDTH+IMG_WIDTH//2, display.get_height()-11*IMG_HEIGHT-IMG_HEIGHT//2)
+above_outlet = (2*IMG_WIDTH+IMG_WIDTH//2, display.get_height()-12*IMG_HEIGHT-IMG_HEIGHT//2)
+
+
 pygame.key.set_repeat(100000,1000)
 
 
@@ -22,6 +26,23 @@ pygame.display.set_caption("puyopuyo")
 # sprite collision
 # move sprites in the group
 
+class FallingPuyos(pygame.sprite.Sprite):
+    def __init__(self, display: pygame.surface, str_puyo1: str, str_puyo2: str):
+        self.display = display
+        self.puyos = [Puyo(self.display, outlet_position, str_puyo1), Puyo(self.display, above_outlet, str_puyo2)]
+    
+    def fall(self):
+        for puyo in self.puyos:
+            puyo.fall()
+    
+    def update(self):
+        for puyo in self.puyos:
+            puyo.update()
+    
+    def draw(self):
+        for puyo in self.puyos:
+            puyo.draw()
+
 
 # single puyo
 class Puyo(pygame.sprite.Sprite):
@@ -30,11 +51,11 @@ class Puyo(pygame.sprite.Sprite):
                "blue":"./img/0_2.png", 
                "yellow":"./img/0_3.png", 
                "purple":"./img/0_4.png"})
-    def __init__(self, color: str, display: pygame.surface) -> None:
+    def __init__(self, display: pygame.surface, center: tuple, color: str) -> None:
         super().__init__()
         self.display = display
         self.image = pygame.image.load(Puyo.to_img[color])
-        self.rect = self.image.get_rect(center = (2*IMG_WIDTH+IMG_WIDTH//2, self.display.get_height()-11*IMG_HEIGHT-IMG_HEIGHT//2))
+        self.rect = self.image.get_rect(center = center)
         
     
     def fall(self) -> None:
@@ -62,30 +83,17 @@ class Puyo(pygame.sprite.Sprite):
 class Window():
     def __init__(self, display: pygame.surface):
         self.display = display
-        # self.left_rect = pygame.Rect((display.get_width()-IMG_WIDTH//2-IMG_WIDTH, display.get_height()-12*IMG_HEIGHT//2+IMG_HEIGHT//2))
         self.left_surface = pygame.Surface((IMG_WIDTH, 2*IMG_HEIGHT))
         self.left_surface.fill((255, 255, 255))
         self.left_rect = self.left_surface.get_rect(topright=(display.get_width()-3*IMG_WIDTH//2, 3*IMG_HEIGHT//2))
         self.right_surface = pygame.Surface((IMG_WIDTH, 2*IMG_HEIGHT))
         self.right_surface.fill((255, 255, 255))
         self.right_rect = self.right_surface.get_rect(topright=(display.get_width()-IMG_WIDTH//2, IMG_HEIGHT//2))
-        # self.left_rect = self.left_surface.get_rect()
 
-    # def draw_puyo_window(surface: pygame.surface):
-    #     center_y = surface.get_height()-12*IMG_HEIGHT//2
-    #     center_x = surface.get_width()-IMG_WIDTH//2
-    #     top1 = center_y+IMG_HEIGHT*3//2
-    #     top2 = center_y+IMG_HEIGHT//2
-    #     bottom1 = center_y-IMG_HEIGHT*3//2
-    #     bottom2 = center_y-IMG_HEIGHT//2
-    #     left = center_x-IMG_WIDTH
-    #     right = center_x+IMG_WIDTH
     def draw(self):
         self.display.blit(self.left_surface, self.left_rect)
         self.display.blit(self.right_surface, self.right_rect)
-    
-# def draw_top(surface: pygame.surface):
-#     pygame.draw.rect()
+
 
 class Frame():
     def __init__(self, surface: pygame.surface):
@@ -105,10 +113,14 @@ class Batsu():
         self.surface.blit(self.image, self.rect)
 
 def main():
-    puyo = Puyo("red", DISPLAY)
-    frame = Frame(DISPLAY)
-    batsu = Batsu(DISPLAY)
-    window = Window(DISPLAY)
+    falling_puyos = FallingPuyos(display, "red", "blue")
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(falling_puyos.puyos[0], falling_puyos.puyos[1])
+    
+    frame = Frame(display)
+    batsu = Batsu(display)
+    window = Window(display)
+    
     while True:
         # catch events
         for event in pygame.event.get():
@@ -117,20 +129,21 @@ def main():
                 pygame.quit()
                 sys.exit()
                 
+        # initiate canvas
+        display.fill("white")
+        
         # update display
-        DISPLAY.fill("white")
+        falling_puyos.update()
+        
+        # draw
+        batsu.draw()
         frame.draw()
         window.draw()
-        batsu.draw()
-        
-        # draw_grid(DISPLAY)
-        # puyo.fall()
-        puyo.update()
-        puyo.draw()
+        falling_puyos.draw()
         
         # render
         pygame.display.update()
-        CLOCK.tick(60)
+        clock.tick(60)
     
 
 if __name__ == "__main__":
