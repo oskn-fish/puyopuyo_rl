f3e087476825d9a63ae951dcf1e9c76476868076
Shunnoske Chinen <flash_wave@icloud.com>
2023-08-12 13:05:43 +0900

    TODO: changed Board.list_board to Board.board

--- puyopuyo/core.py
+++ puyopuyo/core.py
@@ -62,7 +62,7 @@ class FloatingPuyos(pygame.sprite.Sprite):
     def __init__(self, display: pygame.surface, board: Board):
         self.display = display
         self.board = board
-        self.event_flags = {puyo_landed: False}
+        self.event_flags = {PUYO_LANDED: False}
         self.reset_puyos()
         
     def reset_puyos(self):
@@ -90,7 +90,7 @@ class FloatingPuyos(pygame.sprite.Sprite):
     def y_movable(self, dy):
         candidates = [self.puyos[i].rect.move(0, dy) for i in range(2)]
         for candidate in candidates:
-            if candidate.bottom>=self.display.get_height():
+            if candidate.bottom>self.display.get_height():
                 return False
             for puyo in landed_sprites:
                 if pygame.Rect.colliderect(puyo.rect, candidate):
@@ -130,8 +130,9 @@ class FloatingPuyos(pygame.sprite.Sprite):
             landed_sprites.add(self.puyos[0], self.puyos[1])
             # landed_event = pygame.event.Event(puyo_landed, landed_puyos=self.puyos)
             # landed_event.__dict__ = {"landed_puyos":self.puyos}
-            self.event_flags[puyo_landed] = True
-            self.puyos.clear()
+            self.event_flags[PUYO_LANDED] = True
+            self.deleted_puyos = self.puyos
+            self.puyos = []
             # pygame.event.post(landed_event)
         
         for puyo in self.puyos:
@@ -140,10 +141,10 @@ class FloatingPuyos(pygame.sprite.Sprite):
     def post_events(self):
         for event, has_occurred in self.event_flags.items():
             if has_occurred:     
-                if event == puyo_landed:
-                    new_event = pygame.event.Event(event, landed_puyos=self.puyos)
+                if event == PUYO_LANDED:
+                    new_event = pygame.event.Event(event, landed_puyos=self.deleted_puyos)
                     pygame.event.post(new_event)
-                    self.event_flags[puyo_landed] = False
+                    self.event_flags[PUYO_LANDED] = False
     
     def draw(self):
         for puyo in self.puyos:
@@ -158,28 +159,37 @@ class Board():
     
     """
     def __init__(self):
-        self.list_board = np.array([[" "]*6 for i in range(12)])
+        # self.char_list = np.array([[" "]*6 for i in range(12)])
         self.puyos_queue = [[random.choice(COLORS), random.choice(COLORS)] for i in range(2)]
-        
-    def pop_chain_puyos(self, *fell_puyos: dict) -> list:
+        self.board = [[None]*6 for i in range(12)]
+    
+    # for print function
+    def __str__(self):
+        string = "["
+        for i in range(12):
+            string = string+list(map(lambda x: " " if x==None else x.color[0] ,self.board[i]))
+            if i < 11:
+                string += "\n"
+        string += "]"
+        return string
+    
+    def pop_chain_puyos(self, landed_puyos: list[Puyo]) -> bool:
         """
-        
-        fell_puyos = {(i,j): "yellow", ...}
-        
-        used when
-        1. FloatingPuyo lands
-        2. after Pop
-        
-        when 1., (not when 2.)
-        if the color of puyo1 and puyo2 has the same color, 
-        search from only puyo1.
+        remove puyo chains from self.board: list[Puyo]
+        returns if chain pops: bool.        
         """
-        self.list_board_cp = copy.deepcopy(self.list_board)
-        for puyo_coordinate, color in fell_puyos:
-            if self.list_board_cp[puyo_coordinate[0], puyo_coordinate[1]] != "+":
-                longest_chain = self._recursive_chain_search(color, puyo_coordinate)
-                if len(longest_chain) >= 4:
-                    np.char.replace(self.list_board_cp, "+", " ")
+        puyo_coordinates = [self._coord_to_board_idx(puyo.topleft) for puyo in landed_puyos]
+        puyo_colors = [puyo.color for puyo in landed_puyos]
+        does_pop = False
+        for puyo_coordinate, color in zip(puyo_coordinates, puyo_colors):
+            # board for search (searched element will be replaced with "+")
+            self.list_board_cp = copy.deepcopy(self.char_list)
+            longest_chain = self._recursive_chain_search(color, puyo_coordinate)
+            if len(longest_chain) >= 4:
+                does_pop = True
+                np.char.replace(self.list_board_cp, "+", " ")
+                self.char_list = self.list_board_cp
+        return does_pop
 
     
     def _recursive_chain_search(self, puyo_color: str,  puyo_coordinate: dict) -> tuple:
@@ -192,7 +202,7 @@ class Board():
         self.list_board_cp[puyo_coordinate[0]][puyo_coordinate[1]] = "+"
         # is_last_call = True
         for next_coordinate in [(puyo_coordinate[0]+round(math.cos(math.radians(90*i))),puyo_coordinate[1]+round(math.sin(math.radians(90*i)))) for i in range(4)]:
-            if self.list_board[next_coordinate[0]][next_coordinate[1]] == self.list_board[puyo_coordinate[0]][puyo_coordinate[1]]:
+            if self.char_list[next_coordinate[0]][next_coordinate[1]] == self.char_list[puyo_coordinate[0]][puyo_coordinate[1]]:
                 # coordinate_chain.append(puyo_coordinate)
                 coordinate_chain.extend(self._recursive_chain_search(puyo_color, next_coordinate))
                 # is_last_call = False
@@ -205,11 +215,11 @@ class Board():
         puyo_indices = [self._coord_to_board_idx(top_left) for top_left in top_lefts]
         puyo_colors = [puyo.color[0] for puyo in landed_puyos]
         
-        for puyo_index, puyo_color  in zip(puyo_indices,puyo_colors):
-            self.list_board[puyo_index[0] ,puyo_index[1]] = puyo_color
-        
-        # print(self.list_board)
-        # logger.debug(self.list_board)
+        for puyo_index, puyo_color, landed_puyo  in zip(puyo_indices,puyo_colors, landed_puyos):
+            self.char_list[puyo_index[0] ,puyo_index[1]] = puyo_color
+            self.board[puyo_index[0]][puyo_index[1]] = landed_puyo
+        # print(self.char_list)
+        # logger.debug(self.char_list)
 
     
     def _coord_to_board_idx(self, top_left):
